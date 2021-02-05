6d87532886818687880ffa3ecb3acc6e4301bf1b
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-02-05 03:23:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -40,3 +40,12 @@ def printer(array)
     puts assignment
   end 
 end
+
+def printer(array)
+  batch_badge_creator(array).each do |badge|
+    puts badge
+  end
+  assign_rooms(array).each do |assignment|
+    puts assignment
+  end
+end
