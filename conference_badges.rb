6f24c3c5c209143aebbcc809e2bbaaf9b22d93ab
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-02-05 03:33:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,11 @@
+# def badge_maker(name)
+#   "Hello, my name is #{name}."
+# end
+
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
 
 # def batch_badge_creator(array)
 #   array.map do |name|
